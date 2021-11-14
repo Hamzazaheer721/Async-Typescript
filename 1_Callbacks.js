372738c68753cfc58575a66ee6eb80cc6f8c3280
Hamza Zaheer <62650512+Hamzazaheer721@users.noreply.github.com>
2021-11-15 00:14:12 +0500

    !first commit

---
+++ 1_Callbacks.js
@@ -0,0 +1,3 @@
+// a callback is a function that is passed as an arguement to another function
+// A callback function runs after another function has finished
+// a techhnique which allows function to call another function
